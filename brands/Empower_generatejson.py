605a7c16f31b9b4eed3612b662c7d35428db2e2b
autoblack <githubactions@users.noreply.github.com>
2022-03-10 14:15:57 +0000

    Update: autoformat code with black

--- brands/Empower_generatejson.py
+++ brands/Empower_generatejson.py
@@ -11,39 +11,41 @@ date_period = '{"end_date": {"date": "###end_date###"},"start_date": {"date": "#
 
 
 brands = [
-    {"brand_id": 27005, "region": "us", "sector_id": 27},   #"Charles Schwab"
-    {"brand_id": 27008, "region": "us", "sector_id": 27},   #"Edward Jones"
-    {"brand_id": 1003755, "region": "us", "sector_id": 27}, #"Empower Retirement"
-    {"brand_id": 27009, "region": "us", "sector_id": 27},   #"Fidelity"	
-    {"brand_id": 27017, "region": "us", "sector_id": 27},   #"Merrill"
-	{"brand_id": 25017, "region": "us", "sector_id": 25},   #"Nationwide"
-	{"brand_id": 25021, "region": "us", "sector_id": 25},   #"Prudential"
-	{"brand_id": 27021, "region": "us", "sector_id": 27},   #"T. Rowe Price"
-    {"brand_id": 1001310, "region": "us", "sector_id": 27}, #"TIAA"	
-    {"brand_id": 27024, "region": "us", "sector_id": 27},   #"Vanguard"	
-    {"brand_id": 1002056, "region": "us", "sector_id": 27}, #"Voya Financial"	
-    {"brand_id": 1000632, "region": "us", "sector_id": 27}, #"Wells Fargo Advisors"	
-	{"brand_id": 25009, "region": "us", "sector_id": 25},   #"Geico"	
-	{"brand_id": 27015, "region": "us", "sector_id": 25},   #"Mass Mutual"	
-	{"brand_id": 12001, "region": "us", "sector_id": 12},   #"Bank of America"	
-	{"brand_id": 12024, "region": "us", "sector_id": 12},   #"Wells Fargo"	
+    {"brand_id": 27005, "region": "us", "sector_id": 27},  # "Charles Schwab"
+    {"brand_id": 27008, "region": "us", "sector_id": 27},  # "Edward Jones"
+    {"brand_id": 1003755, "region": "us", "sector_id": 27},  # "Empower Retirement"
+    {"brand_id": 27009, "region": "us", "sector_id": 27},  # "Fidelity"
+    {"brand_id": 27017, "region": "us", "sector_id": 27},  # "Merrill"
+    {"brand_id": 25017, "region": "us", "sector_id": 25},  # "Nationwide"
+    {"brand_id": 25021, "region": "us", "sector_id": 25},  # "Prudential"
+    {"brand_id": 27021, "region": "us", "sector_id": 27},  # "T. Rowe Price"
+    {"brand_id": 1001310, "region": "us", "sector_id": 27},  # "TIAA"
+    {"brand_id": 27024, "region": "us", "sector_id": 27},  # "Vanguard"
+    {"brand_id": 1002056, "region": "us", "sector_id": 27},  # "Voya Financial"
+    {"brand_id": 1000632, "region": "us", "sector_id": 27},  # "Wells Fargo Advisors"
+    {"brand_id": 25009, "region": "us", "sector_id": 25},  # "Geico"
+    {"brand_id": 27015, "region": "us", "sector_id": 25},  # "Mass Mutual"
+    {"brand_id": 12001, "region": "us", "sector_id": 12},  # "Bank of America"
+    {"brand_id": 12024, "region": "us", "sector_id": 12},  # "Wells Fargo"
 ]
 
 
 filters = [
     {
-    "segment": "Finance Industry", 
-    "filters": [
-            {"expression": "profiles_us__pdl_industrynaics in [9]"},    
-    ],
-   },
-   {
-    "segment": "IA $50K+", 
-    "filters": [
-            {"expression": "profiles_us__pdl_totalassets_excl_home in [5, 6, 7, 8, 9, 10, 11]"}, 
-    ],
-   },
-   {"segment": "Total Population", "filters": []},
+        "segment": "Finance Industry",
+        "filters": [
+            {"expression": "profiles_us__pdl_industrynaics in [9]"},
+        ],
+    },
+    {
+        "segment": "IA $50K+",
+        "filters": [
+            {
+                "expression": "profiles_us__pdl_totalassets_excl_home in [5, 6, 7, 8, 9, 10, 11]"
+            },
+        ],
+    },
+    {"segment": "Total Population", "filters": []},
 ]
 
 
